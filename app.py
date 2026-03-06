140948c203d1a82a7693cba9c1644cd84a1c5fc8
Felipe Monetti <felipemon6@yahoo.com.ar>
2026-03-06 20:57:40 -0300

    Clean web version

--- app.py
+++ app.py
@@ -278,5 +278,10 @@ def home():
 
 
 # --- Arranque del Servidor ---
+
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Render usa la variable de entorno PORT
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
